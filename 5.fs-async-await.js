6bf22f52704254d5d445044ee2daf6a2dd184584
Emill <lemito66@gmail.com>
2023-10-10 16:49:44 -0500

    corrección de async await

--- 5.fs-async-await.js
+++ 5.fs-async-await.js
@@ -1,19 +1,34 @@
-const fs = require("node:fs/promises");
+// Esto sólo en los módulos nativos
+// que no tienen promesas nativas
 
-fs.readFile("./archivo.txt", "utf-8")
-  .then((data) => {
-    console.log("Primer texto", data);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+// const { promisify } = require('node:util')
+// const readFilePromise = promisify(fs.readFile)
 
-console.log("Hacer cosas mientras lee el archivo");
+const { readFile } = require('node:fs/promises')
 
-fs.readFile("./archivo2.txt", "utf-8")
-  .then((data) => {
-    console.log("Segundo texto", data);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+async function init () {
+  console.log('Leyendo el primer archivo...')
+  const text = await readFile('./archivo.txt', 'utf-8')
+  console.log('primer texto:', text)
+  console.log('--> Hacer cosas mientras lee el archivo...')
+  
+  console.log('Leyendo el segundo archivo...')
+  const secondText = await readFile('./archivo2.txt', 'utf-8')
+  console.log('segundo texto:', secondText)    
+}
+
+init()
+
+// IIFE - Inmediatly Invoked Function Expression
+// ;(
+//   async () => {
+//     console.log('Leyendo el primer archivo...')
+//     const text = await readFile('./archivo.txt', 'utf-8')
+//     console.log('primer texto:', text)
+//     console.log('--> Hacer cosas mientras lee el archivo...')
+    
+//     console.log('Leyendo el segundo archivo...')
+//     const secondText = await readFile('./archivo2.txt', 'utf-8')
+//     console.log('segundo texto:', secondText)    
+//   }
+// )()
